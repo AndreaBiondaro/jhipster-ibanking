a1665426a92abaa683ac402dfc7b3c7786a13de8
AndreaBiondaro <andrea.biondaro89@gmail.com>
2022-03-02 15:59:44 +0000

    Initial version of gateway generated by generator-jhipster@7.7.0

---
+++ gateway/src/main/webapp/app/entities/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import ErrorBoundaryRoute from 'app/shared/error/error-boundary-route';
+
+import Conto from './conti/conto';
+import Bonifico from './bonifici/bonifico';
+/* jhipster-needle-add-route-import - JHipster will add routes here */
+
+const Routes = ({ match }) => (
+  <div>
+    <Switch>
+      {/* prettier-ignore */}
+      <ErrorBoundaryRoute path={`${match.url}conto`} component={Conto} />
+      <ErrorBoundaryRoute path={`${match.url}bonifico`} component={Bonifico} />
+      {/* jhipster-needle-add-route-path - JHipster will add routes here */}
+    </Switch>
+  </div>
+);
+
+export default Routes;
